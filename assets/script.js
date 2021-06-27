22e92574ed95e659023f0630ef972ec8f03d053b
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-06-27 13:44:42 -0400

    Javascript Initials/HTML tweaks

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+    $(".saveBtn").on("click", function () {
+        console.log(this);
+        
+        var text = $(this).siblings(".entry_data").val();
+        var time = $(this).parent().attr("id");
+
+        localStorage.setItem(time,text);
+        
+    })
