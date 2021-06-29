515aed0c11c0311c104aa15178494fc54a230dce
Luke McMurtrie <Lmcmurtrie89@gmail.com>
2021-06-29 14:11:40 -0400

    HTML/Java debugs

--- assets/script.js
+++ assets/script.js
@@ -25,21 +25,28 @@ $(document).ready(function () {
     function hourTracker () {
         var currentHour = moment().hour();
 
-        $(".time-block).each(function () {
+        $(".time-block").each(function () {
             var blockHour = parseInt($(this).attr("id").split("hour")[1]);
             console.log(blockHour, currentHour)
         
-            if (blockHour < currentHour) {
+            if(blockHour <currentHour) {
                 $(this).addClass("past");
-                $(this).removeClass("present");
                 $(this).removeClass("future");
+                $(this).removeClass("present");
             }
-            else if (blockHour === currentHourtr) {
+            else if (blockHour === currentHour) {
                 $(this).removeClass("past");
+                $(this).addClass("present");
                 $(this).removeClass("future");
-                $(this).removeClass("present");
             }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+    })
+
     }
+    hourTracker ();
 
-
-}
+})
